1bd77040380ac78fe4c9fa64f1e8b465e44a9b1d
redcloud442 <cloudred442@gmail.com>
2026-02-05 00:41:48 +0800

    add more optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from "astro/config";
 
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://trbhcares.com",
+  integrations: [react(), sitemap()],
   adapter: netlify(),
 
   vite: {
